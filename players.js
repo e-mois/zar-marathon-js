086550c1dd8f87d03c1c02c9857095009c5a3ed1
Eugene Moiseev <eugen.moiseev@gmail.com>
2021-10-28 03:19:08 +0300

    Homework-8
    Имеются у меня сомнения насчёт main.js, но ничего разумнее и лаконичнее не придумал. Жду комментов!

--- players.js
+++ players.js
@@ -1,46 +1,56 @@
-export const player1 = {
-    number: 1,
-    name: 'Scorpion',
-    hp: 100,
-    img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
-    weapon: ['supershout'],
-    attack: function() {
-        console.log(player1.name + ' ' + 'Fight!');
-    },
-    changeHP,
-    elHP,
-    renderHP,
-}
+export class Player {
+    constructor(props) {
+        this.number = props.number;
+        this.name = props.name;
+        this.hp = props.hp;
+        this.img = props.img;
+    }
 
-export const player2 = {
-    number: 2,
-    name: 'Kitana',
-    hp: 100,
-    img:'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
-    weapon: ['megasupershout'],
-    attack: function() {
-        console.log(player2.name + ' ' + 'Fight!');
-    },
-    changeHP,
-    elHP,
-    renderHP,
-}
+    changeHP(damage) {
 
-function changeHP(damage) {
+        this.hp -= damage;
+        
+        if (this.hp < 0) {
+            this.hp = 0;
+        }
+    }
 
-    this.hp -= damage;
-    
-    if (this.hp < 0) {
-        this.hp = 0;
+    elHP() {
+        const $playerLife = document.querySelector('.player' + this.number + ' .life');
+        return $playerLife
+    }
+
+    renderHP() {
+        this.elHP().style.width = this.hp + '%';
     }
 }
 
-function elHP() {
-    const $playerLife = document.querySelector('.player' + this.number + ' .life');
-    return $playerLife
-}
 
-function renderHP() {
-    this.elHP().style.width = this.hp + '%';
-}
 
+// export const player1 = {
+//     number: 1,
+//     name: 'Scorpion',
+//     hp: 100,
+//     img:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+//     weapon: ['supershout'],
+//     attack: function() {
+//         console.log(player1.name + ' ' + 'Fight!');
+//     },
+//     changeHP,
+//     elHP,
+//     renderHP,
+// }
+
+// export const player2 = {
+//     number: 2,
+//     name: 'Kitana',
+//     hp: 100,
+//     img:'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+//     weapon: ['megasupershout'],
+//     attack: function() {
+//         console.log(player2.name + ' ' + 'Fight!');
+//     },
+//     changeHP,
+//     elHP,
+//     renderHP,
+// }
